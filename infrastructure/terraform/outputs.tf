6e1be503e415cec886f33f31da9b819232b38803
Uday Joshi <ujoshi@captureventures.com>
2026-04-17 11:25:21 -0400

    fix: update value for resource_group_name in outputs.tf

--- infrastructure/terraform/outputs.tf
+++ infrastructure/terraform/outputs.tf
@@ -4,7 +4,7 @@
 
 output "resource_group_name" {
     description = "Name of the Azure resource group"
-    value       = azurerm_resource_group.this.name
+    value       = data.azurerm_resource_group.this.name
 }
 
 output "storage_account_name" {
